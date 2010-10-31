2b9a35fd28fae20700cea447df2ac438d3ee5eee
Johannes Held <mail@hehejo.de>
2010-11-01 00:42:08 +0100

    Impl verbessert

--- listings/pubsub_usage.cpp
+++ listings/pubsub_usage.cpp
@@ -12,8 +12,8 @@ void deliver(const string& node, const MessageT& message) {
 }
 
 int main() {
-	PubSubSystem::getInstance().init(700)
-	IChannel bcast = PubSubSystem::getInstance().getChannelHandle(BROADCAST);
+	PubSubSystem::getInstance().init(7000);
+	IChannel* bcast = PubSubSystem::getInstance().getChannelHandle(BROADCAST);
 	
 	app_deliver_func f = boost::bind(deliver, "[node 1]", _1);
 	PubSubSystem::getInstance().init(7001, "localhost", 7000).subscribe(BROADCAST, f);
@@ -21,7 +21,6 @@ int main() {
 	app_deliver_func f_2 = boost::bind(deliver, "[node 2]", _1);
 	PubSubSystem::getInstance().init(7002, "localhost", 7000).subscribe(BROADCAST, f_2);
 	
-
 	while(true) {
 			bcast->publish("Nachricht an Alle.");
 			boost::this_thread::sleep(boost::posix_time::seconds(5));
